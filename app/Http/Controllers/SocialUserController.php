a3b2a56ebefc30c9a9e5bfac43b61d052e3450d6
A-Zhong <verysimplesetting@gmail.com>
2023-03-27 18:15:48 +0800

    Update SocialUserController.php

--- app/Http/Controllers/SocialUserController.php
+++ app/Http/Controllers/SocialUserController.php
@@ -26,7 +26,7 @@ class SocialUserController extends Controller
 
         config()->set('services.google.redirect', $url);
 
-        $user_data = Socialite::driver('google')->user();
+        $user_data = Socialite::driver('google')->stateless()->user();
 
         // 註冊過直接登入,沒註冊過創建新使用者
 
