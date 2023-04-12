89737380a2617db70a75d9bb2524ab406047b917
A-Zhong <verysimplesetting@gmail.com>
2023-04-12 08:42:28 +0800

    fix: UpperCase

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
-use App\Models\product;
+use App\Models\Product;
 use Ecpay\Sdk\Factories\Factory;
 use Ecpay\Sdk\Services\UrlService;
 use Illuminate\Support\Facades\Auth;
