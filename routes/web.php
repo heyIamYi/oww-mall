513737cf150c726f6bb99643e8f98acc124aae14
A-Zhong <verysimplesetting@gmail.com>
2023-07-16 16:08:30 +0800

    1231

--- routes/web.php
+++ routes/web.php
@@ -176,5 +176,5 @@ Route::get('/google/callback', [SocialUserController::class, 'googlecallback']);
  */
 
 
-Route::post('/track-event', [TrackingController::class, 'trackEvent']);
+Route::get('/track-event', [TrackingController::class, 'trackEvent']);
 
