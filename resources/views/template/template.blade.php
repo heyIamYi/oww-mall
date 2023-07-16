34aaaabfba976a8fccca754fb4917243d477d5ae
A-Zhong <verysimplesetting@gmail.com>
2023-07-16 17:11:36 +0800

    1231231

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -23,44 +23,60 @@
 
     <!-- Meta Pixel Code -->
     <script>
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '520514866883077', {
-         user_id: 'anonymous_user_id'
-    });
-    fbq('track', 'PageView');
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '520514866883077', {
+            user_id: 'anonymous_user_id'
+        });
+        fbq('track', 'PageView');
 
-    snetPageView();
-    // 傳送資料
-    function snetPageView() {
-        let csrfToken = '{{ csrf_token() }}';
-        fetch('/track-event',{
-            method:'post',
-            headers: {
-            'Content-Type': 'application/json',
-            'X-CSRF-TOKEN': csrfToken
-        },
-        })
-        .then(response => {
-            console.log(response);
-            if (response == 'success') {
-                return response;
+        snetPageView();
+        // 傳送資料
+        function snetPageView() {
+            function snetPageView() {
+                let csrfToken = '{{ csrf_token() }}';
+
+                fetch('/track-event', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'X-CSRF-TOKEN': csrfToken
+                        },
+                    })
+                    .then(response => {
+                        console.log(response);
+                        if (response.ok) {
+                            return response.json();
+                        }
+                        throw new Error('請求失敗');
+                    })
+                    .then(data => {
+                        console.log(data);
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
             }
-        })
-        .then(data => {
-            console.log(data);
-        })
-    }
+        }
     </script>
     <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=520514866883077&ev=PageView&noscript=1"
-    /></noscript>
+            src="https://www.facebook.com/tr?id=520514866883077&ev=PageView&noscript=1" /></noscript>
     <!-- End Meta Pixel Code -->
 
 </head>
@@ -69,7 +85,8 @@
     <nav>
         <!-- logo -->
         <div class="container-xxl d-flex justify-content-lg-between ">
-            <div class="logo w-25"><a href="/"><img class="w-100" src="{{ asset('img/logo/logo.png') }}" alt=""></a></div>
+            <div class="logo w-25"><a href="/"><img class="w-100" src="{{ asset('img/logo/logo.png') }}"
+                        alt=""></a></div>
             <!-- 相關超連結 -->
             @auth
 
