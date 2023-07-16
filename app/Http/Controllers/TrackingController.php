741bc6ce8607b2120e008861d88c7724e202349f
A-Zhong <verysimplesetting@gmail.com>
2023-07-16 19:28:32 +0800

    2131

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -9,7 +9,6 @@ class TrackingController extends Controller
 {
     public function trackEvent(Request $request)
     {
-        dd(231);
         $pixelId = config('app.facebook_pixel_id');
         $accessToken = config('app.facebook_access_token');
 
